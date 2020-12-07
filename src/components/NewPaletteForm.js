b7537e3cbfe890030d634d05f4770bb953b05ed3
changangus <63685366+changangus@users.noreply.github.com>
2020-12-07 13:24:19 -0500

    added components to drawer on new palette page

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -10,8 +11,9 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ChromePicker } from 'react-color';
 
-const drawerWidth = 240;
+const drawerWidth = 340;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -122,6 +124,19 @@ export function NewPaletteForm() {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant="h4">Design Your Palette!</Typography>
+        <div>
+          <Button variant='contained' color='primary'>Random Color</Button>
+          <Button variant='contained' color='secondary'>Clear Palette</Button>
+        </div>
+        <ChromePicker 
+          color='purple'
+          onChange={() => {}}
+          onChangeComplete={(newColor) => {
+            console.log(newColor)
+          }}
+          />
+          <Button variant='contained' color='blue'>Add Color</Button>
       </Drawer>
       <main
         className={clsx(classes.content, {
